980ceaaf0ae54b5a42a9dfd40c6aa9e458ec718e
Simon Dunton <simon@wpsites.co.uk>
2012-06-19 21:49:56 +0000

    Added the functionality to share content from template/mastersite so that common conent can be managed in a single location

--- WPMU-site-clone.php
+++ WPMU-site-clone.php
@@ -76,4 +76,193 @@ function wpmusc_admin_page() {
 	// include the adminpage
 	include('adminpage-add-cloned-sites.php');
 }
+
+
+/*
+ * replace page content with template site if featured enabled
+*/
+
+//main function to set actions/filters
+function try_use_parent_content(){
+  $linked_to_parent = get_option('add-cloned-sites-linked-pages');
+  if ($linked_to_parent){
+      
+        add_filter( 'the_title', 'my_title_filter_parent', 1, 2); //title
+        add_filter( 'the_content', 'my_the_content_filter_parent', 1 ); //content
+        
+        add_action('do_meta_boxes', 'wpmusc_topof_pageedit_screen', 10, 3);  //output message to page edit screen
+        add_action('pre_post_update', 'wpmusc_doc_make_master'); //make this page a master when it gets edited
+        
+  }
+}
+add_action('init', 'try_use_parent_content'); 
+add_action('admin_init', 'try_use_parent_content'); 
+
+
+// content
+function my_the_content_filter_parent($content) { 
+  
+    if ( wpmusc_doc_is_master() ){
+       return $content; //return this doc
+    }else{
+       
+        if ( is_array( $from_master = wpmusc_doc_from_master() ) ) {
+          
+            return $from_master['post_content'];
+            
+        }else{ //fallback on title passed in
+            return $content;
+        }
+       
+    }
+  
+}
+
+//title
+function my_title_filter_parent($title, $id) {
+  
+    //if this page is the master then do nothing. Otherwise get the page content from the master/template site
+    if ( wpmusc_doc_is_master($id) ){
+       return $title; //return this doc
+    }else{
+       
+        if ( is_array( $from_master = wpmusc_doc_from_master($id) ) ) {
+          
+            return $from_master['post_title'];
+            
+        }else{ //fallback on title passed in
+            return $title;
+        }
+       
+    }
+   
+}
+
+
+
+//get the master doc from the template site
+function wpmusc_doc_from_master($id=''){
+   
+    if ($id>0){
+        $post_id = $id;
+    }else{
+        global $post;
+        $post_id = $post->ID;
+    }
+    
+    if ( isset($GLOBALS['wpmusc_doc_from_master']['ID'] ) && $GLOBALS['wpmusc_doc_from_master']['ID'] == $post_id ){
+        
+        return $GLOBALS['wpmusc_doc_from_master']; //use stored version
+        
+    }else{
+        global $wpdb;
+        
+        $master_blog_info = unserialize(get_option('add-cloned-sites'));
+        
+        $post_table_name = str_replace($wpdb->blogid, $master_blog_info['template-id'], $wpdb->posts); //replace current blog id with master id to get correct table name
+        
+        $master_query = "SELECT * FROM {$post_table_name} ".
+                        "WHERE post_type='page' AND ID='{$post_id}'; ";
+        
+        $GLOBALS['wpmusc_doc_from_master'] = $wpdb->get_row($master_query, ARRAY_A); //use the master version
+        
+        return $GLOBALS['wpmusc_doc_from_master'];
+    }
+    
+}    
+
+//check this page is master
+function wpmusc_doc_is_master($id=''){
+    
+    if ($id>0){
+        $post_id = $id;
+    }else{
+        global $post;
+        $post_id = $post->ID;
+    }
+    
+    return get_post_meta($post_id, 'wpmusc_doc_is_master', true);  
+}
+
+
+//remove quick edit link
+function wpmusc_admin_pagelist( $actions ) {
+ global $post;
+ 
+ if ( wpmusc_doc_is_master($post->ID) ){ //if this page is a master then allow quick edit, otherwise remove
+    
+
+ 
+ }else{
+     unset( $actions['inline hide-if-no-js'] ); //remove quick edit
+ }
+ 
+ return $actions;
+}
+add_filter('page_row_actions','wpmusc_admin_pagelist',10,1);
+
+
+//indicate if the page is linked, in the page list (edit.php)
+function wpmusc_admin_pagelist_linked_indicator( $h_time, $post ) {
+
+ if ( wpmusc_doc_is_master($post->ID) ){ //if this page is a master then allow quick edit, otherwise remove
+    return $h_time;
+ }else{
+     return $h_time . "<br /><strong>" . __('Linked to master &#8734;', 'wpmusc_trdom') . "</strong>";
+ }
+ 
+ return $actions;
+}
+add_filter('post_date_column_time','wpmusc_admin_pagelist_linked_indicator',50,2);
+
+
+function wpmusc_topof_pageedit_screen($post_type, $pos, $post){
+    
+    if ($pos === 'normal'){
+        if ( wpmusc_doc_is_master($post->ID) ){ 
+            //do nothing as this page is the master
+        }else{
+            
+            //show message to say this page is associated to the master page
+            showMessage( __('This page is inked to a master version &#8734;', 'wpmusc_trdom').
+                            '<br />' . __('If you make changes to the contents of this page, this page will no longer be associated to the master.', 'wpmusc_trdom')
+                            , true);
+        }
+    }
+}
+
+function wpmusc_doc_make_master($id) {
+
+    if ( wpmusc_doc_is_master() ){
+       //do nothing this page is already a master
+    }else{
+       
+        //give this page some meta to mark it as a master
+       add_post_meta($id, 'wpmusc_doc_is_master', true);  
+    }
+}
+
+
+/**
+ * Generic function to show a message to the user using WP's 
+ * standard CSS classes to make use of the already-defined
+ * message colour scheme.
+ *
+ * @param $message The message you want to tell the user.
+ * @param $errormsg If true, the message is an error, so use 
+ * the red message style. If false, the message is a status 
+  * message, so use the yellow information message style.
+ */
+function showMessage($message, $errormsg = false)
+{
+    if ($errormsg) {
+		echo '<div id="message" class="error">';
+	}
+	else {
+		echo '<div id="message" class="updated fade">';
+	}
+
+    echo $message . "</div>";
+}
+
 ?>
